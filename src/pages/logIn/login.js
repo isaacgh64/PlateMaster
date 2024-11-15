add6066a7e34f2b5ca0e38a8df40957a34204f0a
isaacgh64 <isaacgh64@gmail.com>
2024-11-15 12:58:12 +0100

    [0.0.1-alpha] - 2024-11-15 ###Added -Login home header and footer

--- src/pages/logIn/login.js
+++ src/pages/logIn/login.js
@@ -1,10 +1,4 @@
-loadHeader();
-//Función que nos permite cargar nuestra cabecera al visitar nuestra página
-function loadHeader(){
-    fetch("../../widgets/header/header.html").then(response => response.text()).then(data=>{
-        document.getElementById("header").innerHTML=data;
-    })
-}
+
 
 //Comprobamos que mandamos los datos con AJAX y hacemos las comprobaciones
 document.getElementById("logInForm").addEventListener("submit",function(event){
